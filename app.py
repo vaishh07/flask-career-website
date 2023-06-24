f46a5f90ab2f5246340f49333ed2d8ea13edaf7c
vaishh07 <97730981+vaishh07@users.noreply.github.com>
2023-06-24 18:07:15 +0000

    added styling to the project

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template,json
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id':1,
+    'title':"Data Analyst I",
+    'location':"Bengaluru,India",
+  },
+  {
+    'id':2,
+    'title':"Data Analyst II",
+    'location':"Bengaluru,India",
+  },
+  {
+    'id':3,
+    'title':"Data Analyst III",
+    'location':"Bengaluru,India",
+  },
+  {
+    'id':4,
+    'title':"Data Scientist",
+    'location':"Bengaluru,India",
+  }
+]
+
 @app.route("/")
 def index():
-  return "Hello World"
+  return render_template('home.html',jobs=JOBS)
+  
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
