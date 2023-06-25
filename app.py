a30c2ef7bef14eafc8ff82da3db932d21e1d32f3
vaishh07 <97730981+vaishh07@users.noreply.github.com>
2023-06-25 05:39:04 +0000

    added API route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,json
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -7,27 +7,35 @@ JOBS = [
     'id':1,
     'title':"Data Analyst I",
     'location':"Bengaluru,India",
+    'salary':"Rs. 15,00,000"
   },
   {
     'id':2,
-    'title':"Data Analyst II",
-    'location':"Bengaluru,India",
+    'title':"Data Engineer ",
+    'location':"Pune,India",
+    'salary':"Rs. 13,00,000"
   },
   {
     'id':3,
-    'title':"Data Analyst III",
-    'location':"Bengaluru,India",
+    'title':"Business Analyst",
+    'location':"Hyderabad,India",
+    'salary':"Rs. 20,00,000"
   },
   {
     'id':4,
     'title':"Data Scientist",
     'location':"Bengaluru,India",
+    'salary':"Rs. 25,00,000"
   }
 ]
 
 @app.route("/")
 def index():
-  return render_template('home.html',jobs=JOBS)
+  return render_template('home.html',jobs=JOBS,company_name="Jovian")
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
   
 
 if __name__ == '__main__':
